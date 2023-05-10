a19718f437861e5e0985289fa1ce7280a628616b
ZikaZaki <17160061@su.edu.ye>
2023-05-11 01:59:12 +0300

    add UserContainer redux files

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,11 +1,41 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { connect } from "react-redux"
+import { fetchUsers } from "../redux"
 
-const UserContainer = () => {
-    return (
+const UserContainer = ({ userData, fetchUsers }) => {
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    return userData.loading ? (
+        <h2>Loading...</h2>
+    ) : userData.error ? (
+        <h2>{userData.error}</h2>
+    ) : (
         <div>
-            Users
+            <h2>User List</h2>
+            <div>
+                {userData &&
+                  userData.users &&
+                  userData.users.map(user => <p key={user.id}>{user.name}</p>)
+                }
+            </div>
         </div>
     )
 }
 
-export default UserContainer
+const mapStateToProps = state => {
+    return {
+        userData: state.user
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchUsers: () => dispatch(fetchUsers())
+    }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserContainer)
--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import {
     FETCH_USERS_REQUEST,
     FETCH_USERS_SUCCESS,
@@ -10,16 +11,31 @@ export const fetchUsersRequest = () => {
     }
 }
 
-export const fetchUsersSuccess = users => {
+const fetchUsersSuccess = users => {
     return {
         type: FETCH_USERS_SUCCESS,
         payload: users
     }
 }
 
-export const fetchUsersFailure = error => {
+const fetchUsersFailure = error => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
+    }
+}
+
+export const fetchUsers = () => {
+    return dispatch => {
+        dispatch(fetchUsersRequest)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                const users = response.data
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                const errorMsg = error.message
+                dispatch(fetchUsersFailure(errorMsg))
+            })
     }
 }
