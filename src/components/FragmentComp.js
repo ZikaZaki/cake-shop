599ada68bc0a13d54d0a525b2b99f613262d4a5a
ZikaZaki <17160061@su.edu.ye>
2023-05-15 00:31:13 +0300

    add FragmentComp component

---
+++ src/components/FragmentComp.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const FragmentComp = () => {
+    return (
+        <>
+        
+            <div>FragmentComp</div>
+            <p>Details about FragmentComp</p>
+        </>
+    )
+}
+
+export default FragmentComp
