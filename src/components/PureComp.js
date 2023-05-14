96b0899d8b0bda54a80c7aa5200f01cf58b0fc01
ZikaZaki <17160061@su.edu.ye>
2023-05-15 01:20:28 +0300

    update PureComp

--- src/components/PureComp.js
+++ src/components/PureComp.js
@@ -3,6 +3,9 @@ import React, { PureComponent } from "react"
 // Remember to NOT mutate OBJECTS or ARRAYS in state or props when using PureComponent,
 // because it does a shallow comparison of state and props.
 // Instead make copies of those objects or arrays, using the spread operator or Object.assign().
+// =================================================================================================
+// NOTE: It's a best-practice to ensure that all children of PureComponent are also PureComponents.
+// =================================================================================================
 class PureComp extends PureComponent {
   render() {
     console.log("Pure Component Render")
