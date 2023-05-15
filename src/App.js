fb55f3249576c875fc8decd122083546b2c58bd1
ZikaZaki <17160061@su.edu.ye>
2023-05-16 00:07:36 +0300

    Add FocusInput, Input components

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ import Table from "./components/Table";
 import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <RefsDemo />
+        <FocusInput />
+        {/* <RefsDemo /> */}
         {/* <ParentComp /> */}
         {/* <FragmentComp /> */}
         {/* <Table /> */}
