1bb6800b1687324b64c8ebf87f4cc628620d83a3
ZikaZaki <17160061@su.edu.ye>
2023-05-15 23:30:12 +0300

    Add RefsDemo.js component

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ import FragmentComp from "./components/FragmentComp";
 import Table from "./components/Table";
 import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ParentComp />
+        <RefsDemo />
+        {/* <ParentComp /> */}
         {/* <FragmentComp /> */}
         {/* <Table /> */}
         {/* <LifecycleA /> */}
