331fd128e8c9cf292f43aaba0dee98485362cd09
ZikaZaki <17160061@su.edu.ye>
2023-05-16 23:09:54 +0300

    Update ErrorBoundary component

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react"
-
+// ================== Error Boundary =============================================================
+// Error Boundaries are React components that catch JavaScript error in their child component tree,
+// log those errors, and display a fall-back UIy a fallback UI instead of the component tree that 
+// crashed. A class component becomes an Error Boundary if it defines either (or both) of the
+// lifecycle methods static getDerivedStateFromError(error) or componentDidCatch(error, info).
+// NOTE: Error boundaries catch errors during rendering, in lifecycle methods, and in constructors
+// of the whole tree below them, but NOT errors in the Event Handlers.
+// ===============================================================================================
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props)
@@ -13,6 +20,11 @@ class ErrorBoundary extends Component {
     return {
         hasError: true
     }
+  }
+
+  componentDidCatch(error, info) {
+    console.log('Error msg from componentDidCatch: ', error)
+    console.log('Error info from componentDidCatch: ', info)
   }
 
   render() {
