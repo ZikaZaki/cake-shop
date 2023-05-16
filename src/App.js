73268937bf79f3af0332211db402b8ebb94da40c
ZikaZaki <17160061@su.edu.ye>
2023-05-17 00:46:36 +0300

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -21,12 +21,16 @@ import FRParentInput from "./components/FRParentInput";
 import PortalDemo from "./components/PortalDemo";
 import Hero from "./components/Hero";
 import ErrorBoundary from "./components/ErrorBoundary";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ErrorBoundary>
+        <ClickCounter />
+        <HoverCounter />
+        {/* <ErrorBoundary>
           <Hero heroName="Batman" />
         </ErrorBoundary>
         <ErrorBoundary>
@@ -34,7 +38,7 @@ function App() {
         </ErrorBoundary>
         <ErrorBoundary>
           <Hero heroName="Joker" />
-        </ErrorBoundary>
+        </ErrorBoundary> */}
         {/* <PortalDemo /> */}
         {/* <FRParentInput /> */}
         {/* <FocusInput /> */}
