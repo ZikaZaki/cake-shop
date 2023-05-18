dfa36ea129fde21869340766be1f42d685be0c54
ZikaZaki <17160061@su.edu.ye>
2023-05-19 02:07:12 +0300

    documented CompE component

--- src/components/CompE.js
+++ src/components/CompE.js
@@ -3,6 +3,9 @@ import CompF from "./CompF"
 import UserContext from "./userContext"
 
 class CompE extends Component {
+  
+  static contextType = UserContext
+
   render() {
     return (
         <div>
@@ -12,7 +15,17 @@ class CompE extends Component {
     )
   }
 }
+// ================================================================================
+// NOTE: Instead of using the below line, we can use the public static contextType
+// property on the class to consume the context.
+// --------------------------------------------------------------------------------
+// NOTE: There are two limitations to using the contextType property:
+// 1. It can only be used with class components.
+// 2. You can only subscribe to a single context using contextType.
+// --------------------------------------------------------------------------------
+// RECOMMENDED: Use the useContext hook instead. See CompF.js for an example.
+// ================================================================================
 
-CompE.contextType = UserContext
+// CompE.contextType = UserContext
 
 export default CompE
