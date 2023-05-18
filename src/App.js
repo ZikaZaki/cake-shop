494ebec5e17f88e60b82321553f1651582b75193
ZikaZaki <17160061@su.edu.ye>
2023-05-19 01:19:18 +0300

    add CompA component

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ function App() {
 
         {/* <ClickCounterTwo />
         <HoverCounterTwo /> */}
+        
         <User render={(isLoggedIn) => isLoggedIn ? 'ZikaZaki' : 'Guest'} />
         {/* <ClickCounter name='ZikaZaki' /> */}
         {/* <HoverCounter name='ZikaZaki'/> */}
