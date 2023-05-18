6748fa12aa8f2e068aae0e1d8fca7014710b2bad
ZikaZaki <17160061@su.edu.ye>
2023-05-19 01:55:47 +0300

    documented CompF compoennt

--- src/components/CompF.js
+++ src/components/CompF.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react"
 import { UserConsumer } from "./userContext"
+// ================================================================================
+// NOTE: There are 2 ways to consume context in React. The first way is to use the
+// Consumer component from React. The second way is to use the useContext hook.
+// Example of using the useContext hook:--------------------------------------------
+// import React, { useContext } from "react"
+// import { UserContext } from "./userContext"
+// 
+// const user = useContext(UserContext) { return <div>Hi {user} from CompF</div> }
+// ---------------------------------------------------------------------------------
+// ================================================================================
 
 class CompF extends Component {
   render() {
