0f123b44441d5eb498030c9a9a63dffb11f70bfe
ZikaZaki <17160061@su.edu.ye>
2023-05-18 23:18:33 +0300

    Add User component

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        ZikaZaki
+      </div>
+    )
+  }
+}
+
+export default User
