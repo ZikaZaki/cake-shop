c5878efe0ea37b101551b18db92de961bf04d0df
ZikaZaki <17160061@su.edu.ye>
2023-05-19 01:58:34 +0300

    update CompE with UserContext

--- src/components/CompE.js
+++ src/components/CompE.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react"
 import CompF from "./CompF"
+import UserContext from "./userContext"
 
 class CompE extends Component {
   render() {
-    return <CompF />
+    return (
+        <div>
+            Component E Context {this.context}
+            <CompF />
+        </div>
+    )
   }
 }
+
+CompE.contextType = UserContext
 
 export default CompE
