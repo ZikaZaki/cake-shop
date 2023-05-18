1af0b2fc0f1077ff97ca21113023c371d9d63586
ZikaZaki <17160061@su.edu.ye>
2023-05-19 00:33:20 +0300

    update User component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react"
-// ===================================RENDER PROPS===================================
-// NOTE: The term "render prop" refers to a technique for sharing code between
-// React components using a prop whose value is a function.
-// ==================================================================================
+
 class User extends Component {
   render() {
     return (
