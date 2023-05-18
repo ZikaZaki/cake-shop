26df9186ca6823b18fc474d96db1c8591bb3e9f7
ZikaZaki <17160061@su.edu.ye>
2023-05-18 23:48:00 +0300

    documented User component

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react"
-
+// ===================================RENDER PROPS===================================
+// NOTE: The term "render prop" refers to a technique for sharing code between
+// React components using a prop whose value is a function.
+// ==================================================================================
 class User extends Component {
   render() {
     return (
