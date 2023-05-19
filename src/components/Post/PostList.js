6b79fb474392546258aa7bb00f8e85f91bf87978
ZikaZaki <17160061@su.edu.ye>
2023-05-20 01:32:13 +0300

    rendered posts in PosList

--- src/components/Post/PostList.js
+++ src/components/Post/PostList.js
@@ -14,6 +14,7 @@ class PostList extends Component {
     axios.get("https://jsonplaceholder.typicode.com/posts")
       .then(response => {
         console.log(response)
+        this.setState({posts: response.data})
       })
       .catch(error => {
         console.log(error)
@@ -21,9 +22,15 @@ class PostList extends Component {
   }
 
   render() {
+    const { posts } = this.state
     return (
       <div>
         List of posts
+        {
+          posts.length ?
+          posts.map(post => <div key={post.id}>{post.title}</div>) :
+          null
+        }
       </div>
     )
   }
